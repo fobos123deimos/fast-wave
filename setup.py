57e3c41eb59187a704f30fe74d9006c58c48a6a4
Matheus Cordeiro <matheusgomescord@gmail.com>
2024-07-15 20:37:23 -0300

    setup files

---
+++ setup.py
@@ -0,0 +1,44 @@
+import setuptools
+from setuptools import find_packages
+
+name = "fast_wave"
+version = "1.0.0"
+description = "Package for the calculation of the time-independent wavefunction."  
+author_email = "matheusgomescord@gmail.com"
+url = "https://github.com/pikachu123deimos/fast-wave" 
+
+install_requires = [
+    "llvmlite==0.42.0",
+    "mpmath==1.3.0",
+    "numba==0.59.1",
+    "numpy==1.26.4",
+    "scipy==1.13.0",
+    "sympy==1.12",
+]
+
+test_requires = [
+    "pytest= ^7.1.2",
+]
+
+packages = find_packages(where='src')
+
+classifiers = [
+    'Programming Language :: Python :: 3.11.5',  
+    'Topic :: Scientific/Computational Mathematics',  
+    'License :: OSI Approved :: BSD License',  
+    'Development Status :: 5 - Stable', 
+]
+
+setuptools.setup(
+    name=name,
+    version=version,
+    description=description,
+    author=name,
+    author_email=author_email,
+    url=url,
+    install_requires=install_requires,
+    test_requires=test_requires,
+    packages=packages,
+    package_dir={'': 'src'},
+    classifiers=classifiers,
+)
